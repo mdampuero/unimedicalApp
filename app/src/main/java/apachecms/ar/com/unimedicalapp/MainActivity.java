803cff2525080732ef88be29e6214519d895a998
Mauricio Ampuero <mdampuero@gmail.com>
2020-01-31 16:01:21 -0300

    Initial

---
+++ app/src/main/java/apachecms/ar/com/unimedicalapp/MainActivity.java
@@ -0,0 +1,14 @@
+package apachecms.ar.com.unimedicalapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
